df54ca4bbdd349064903d2543ca2f2eacd0ff0fc
玄星 <2540149177@qq.com>
2024-04-23 21:13:53 +0800

    feat: 优化文件结构，添加经验瓶

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -1,10 +1,20 @@
 extends CanvasLayer
 
-
 @export var arena_time_manager: Node
+
+@onready var label = $%Label as Label
 
 func _process(delta):
     if arena_time_manager == null:
         return
     
     var timer = arena_time_manager.get_time_elapsed()
+
+    label.text = format_seconds_to_string(timer)
+
+
+func format_seconds_to_string(seconds: float):
+    var minutes= floor(seconds / 60)
+    var remaining_seconds = floor(seconds - minutes * 60)
+
+    return str(minutes) + ':'+ str("%02d" % remaining_seconds)
