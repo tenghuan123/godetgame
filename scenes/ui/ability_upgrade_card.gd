b3b54ef6301bb16d8658f5aa499156905e15c677
玄星 <2540149177@qq.com>
2024-05-13 17:17:56 +0800

    feat: Enabling upgrade Selection

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,7 +1,19 @@
 extends PanelContainer
 
+# 定义一个鼠标点击后发出的事件
+signal selected
+
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+
+func _ready():
+    gui_input.connect(on_gui_input)
+
+
+func on_gui_input(event: InputEvent): 
+    if(Input.get_action_strength('mouse_left_click')):
+        selected.emit()
+
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
