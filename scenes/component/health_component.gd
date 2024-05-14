779f6d9aa5c1abefefaa2ac25f4ac12d46e829ff
玄星 <2540149177@qq.com>
2024-05-14 21:06:21 +0800

    feat: 21.Player Health Bar

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -6,6 +6,8 @@ var current_health
 # 死亡信号
 signal died
 
+signal current_health_change
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     current_health = max_health
@@ -13,6 +15,7 @@ func _ready():
 
 func damage(damage_count:float):
     current_health = max(current_health - damage_count, 0)
+    current_health_change.emit()
     Callable(check_died).call_deferred()
 
 func check_died():
@@ -20,3 +23,9 @@ func check_died():
         died.emit()
         # 这里的owner类似于 js中的this
         owner.queue_free()
+
+func get_health_percent():
+    if(max_health == 0):
+        return 0
+
+    return min(1, current_health / max_health)
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -6,48 +6,59 @@ const ACCELERATION_SMOOTHING = 25
 var number_colliding_bodies = 0
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CollisionArea2D.body_entered.connect(on_body_entered)
-	$CollisionArea2D.body_exited.connect(on_body_exited)
-	damage_interval_timer.timeout.connect(on_damage_interval_timeout)
+    $CollisionArea2D.body_entered.connect(on_body_entered)
+    $CollisionArea2D.body_exited.connect(on_body_exited)
+    damage_interval_timer.timeout.connect(on_damage_interval_timeout)
+    health_component.current_health_change.connect(on_current_health_change)
+    update_health_display()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var movement = get_movement_player()
-	var direction = movement.normalized()
-	var targer_velocity = direction * MAX_SPEED
-	velocity = velocity.lerp(targer_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	move_and_slide()
-	
-
+    var movement = get_movement_player()
+    var direction = movement.normalized()
+    var targer_velocity = direction * MAX_SPEED
+    velocity = velocity.lerp(targer_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+    move_and_slide()
+    
 
 func get_movement_player():
-	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	return Vector2(x, y)
+    var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+    var y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+    
+    return Vector2(x, y)
 
-
-func on_body_entered(other_body: Node2D):
-	number_colliding_bodies += 1
-	check_deal_damage()
-
-
-func on_body_exited(other_body: Node2D):
-	number_colliding_bodies -= 1
 
 
 func check_deal_damage():
-	if(number_colliding_bodies == 0 || !damage_interval_timer.is_stopped()):
-		return
-	
-	$HealthComponent.damage(1)
-	damage_interval_timer.start()
-	print($HealthComponent.current_health)
+    if(number_colliding_bodies == 0 || !damage_interval_timer.is_stopped()):
+        return
+    
+    health_component.damage(1)
+    damage_interval_timer.start()
 
 
-func  on_damage_interval_timeout():
-	check_deal_damage()
+func update_health_display():
+    health_bar.value = health_component.get_health_percent()
+
+
+func on_damage_interval_timeout():
+    check_deal_damage()
+
+func on_body_entered(other_body: Node2D):
+    number_colliding_bodies += 1
+    check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+    number_colliding_bodies -= 1
+
+
+func  on_current_health_change():
+    update_health_display()
