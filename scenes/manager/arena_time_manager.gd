4ee904316d1b1028dbb9eb52c519abe8f1d93d1a
玄星 <2540149177@qq.com>
2024-05-15 13:46:12 +0800

    feat: 22.Creating the Victory Screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,9 +1,21 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
+
 @onready var timer = $Timer
 
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
+
 func get_time_elapsed(): 
-    if timer == null:
-        return
-    # 这里大概的意思就是总时间减去定时器剩余的时间，就等于过去了多久时间
-    return timer.wait_time - timer.time_left
+	if timer == null:
+		return
+	# 这里大概的意思就是总时间减去定时器剩余的时间，就等于过去了多久时间
+	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	print(on_timer_timeout)
+	add_child(victory_screen_instance)
