de8c85fc8486d5ab53dfa7721ec22d5b1c2ae2bf
玄星 <2540149177@qq.com>
2024-05-18 17:51:51 +0800

    6,Prevent Abilities from Being Chosen

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -7,43 +7,51 @@ extends Node
 var current_upgrades = {}
 
 func _ready():
-	(experience_manager as ExperienceManager).level_up.connect(on_level_up)
-	
+    (experience_manager as ExperienceManager).level_up.connect(on_level_up)
+    
 
 
 func apply_upgrade(upgrade: AbilityUpgrade):
-	var has_upgrade = current_upgrades.has(upgrade.id)
-	if !has_upgrade:
-		current_upgrades[upgrade.id] = {
-			"resource": upgrade,
-			"quantity": 1
-		}
-	else:
-		current_upgrades[upgrade.id]["quantity"] += 1
+    var has_upgrade = current_upgrades.has(upgrade.id)
+    if !has_upgrade:
+        current_upgrades[upgrade.id] = {
+            "resource": upgrade,
+            "quantity": 1
+        }
+    else:
+        current_upgrades[upgrade.id]["quantity"] += 1
 
-	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+    if(upgrade.max_quantity > 0):
+        var current_quantity = current_upgrades[upgrade.id]['quantity']
+        if(current_quantity == upgrade.max_quantity):
+            upgrade_pool = upgrade_pool.filter(func(pool_upgrade): return pool_upgrade.id != upgrade.id)
+
+    GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
 func pick_upgrades():
-	var chosen_upgrades = [] as Array[AbilityUpgrade]
-	var filtered_upgrades = upgrade_pool.duplicate()
+    var chosen_upgrades = [] as Array[AbilityUpgrade]
+    var filtered_upgrades = upgrade_pool.duplicate()
 
-	for i in 2:
-		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
-		chosen_upgrades.append(chosen_upgrade)
-		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+    for i in 2:
+        if(filtered_upgrades.size() == 0):
+            break
 
-	return chosen_upgrades
+        var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+        chosen_upgrades.append(chosen_upgrade)
+        filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+
+    return chosen_upgrades
 
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
-	apply_upgrade(upgrade)
+    apply_upgrade(upgrade)
 
 
 func on_level_up(current_level: int):
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	var chosen_upgrades = pick_upgrades()
-	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades)
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+    var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+    add_child(upgrade_screen_instance)
+    var chosen_upgrades = pick_upgrades()
+    upgrade_screen_instance.set_ability_upgrades(chosen_upgrades)
+    upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
