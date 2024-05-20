8d3bb881d2072f1340d58caa8b7ae8589ddb2382
玄星 <2540149177@qq.com>
2024-05-20 17:57:20 +0800

    11.Using a Weighted Table for Enemy Spawing

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
@@ -8,51 +9,62 @@ extends Node
 const SPAWN_RADIUS = 350
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	base_spawn_time = timer.wait_time
-	timer.timeout.connect(on_timer_timeout)
-	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
+    enemy_table.add_item(basic_enemy_scene, 10)
+
+    base_spawn_time = timer.wait_time
+    timer.timeout.connect(on_timer_timeout)
+    arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
 func get_spawn_position():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player == null:
-		return Vector2.ZERO
+    var player = get_tree().get_first_node_in_group("player") as Node2D
+    if player == null:
+        return Vector2.ZERO
 
-	var spawn_position = Vector2.ZERO
-	# 这个TAU就是 2排
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+    var spawn_position = Vector2.ZERO
+    # 这个TAU就是 2排
+    var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
 
-	for i in 4:
-		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+    for i in 4:
+        spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
 
-		var query_paramaters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
-		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramaters)
+        var query_paramaters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+        var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramaters)
 
-		if(result.is_empty()):
-			break
-		else:
-			random_direction  = random_direction.rotated(rad_to_deg(90))
+        if(result.is_empty()):
+            break
+        else:
+            random_direction  = random_direction.rotated(rad_to_deg(90))
 
-	return spawn_position
+    return spawn_position
 
 
 func on_timer_timeout():
-	timer.start()
-	
+    timer.start()
 
-	var enemy = basic_enemy_scene.instantiate() as Node2D
-	# 这里需要将敌人渲染到实体层
-	var entities_layer = get_tree().get_first_node_in_group("entities_layer") as Node2D
-	entities_layer.add_child(enemy)
-	
-	enemy.global_position = get_spawn_position()
+    var player = get_tree().get_first_node_in_group("player") as Node2D
+    if(player == null):
+        return 
+
+    var enemy_scene = enemy_table.pick_item()
+    var enemy = enemy_scene.instantiate() as Node2D
+
+    # 这里需要将敌人渲染到实体层
+    var entities_layer = get_tree().get_first_node_in_group("entities_layer") as Node2D
+    entities_layer.add_child(enemy)
+    
+    enemy.global_position = get_spawn_position()
 
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
-	var time_off = (.1 / 12) * arena_difficulty
-	time_off = min(time_off, .7)
-	timer.wait_time = base_spawn_time - time_off
+    var time_off = (.1 / 12) * arena_difficulty
+    time_off = min(time_off, .7)
+    timer.wait_time = base_spawn_time - time_off
+
+    if arena_difficulty == 6:
+        enemy_table.add_item(wizard_enemy_scene, 20)
