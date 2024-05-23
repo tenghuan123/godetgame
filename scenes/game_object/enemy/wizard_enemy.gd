309f486f349d2c792e2ea0da91d3045789286ef0
玄星 <2540149177@qq.com>
2024-05-23 16:17:24 +0800

    12. Animating the Wizar

--- scenes/game_object/enemy/wizard_enemy.gd
+++ scenes/game_object/enemy/wizard_enemy.gd
@@ -3,19 +3,28 @@ extends CharacterBody2D
 @onready var visuals = $Visuals
 @onready var velocity_component = $VelocityComponent
 
+var is_moving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	velocity_component.accelerate_to_player()
-	velocity_component.move(self)
+    if(is_moving):
+        velocity_component.accelerate_to_player()
+    else:
+        velocity_component.decelerate()
 
-	var move_sign = sign(velocity.x)
-	if move_sign != 0:
-		visuals.scale = Vector2(move_sign, 1)
+    velocity_component.move(self)
+
+    var move_sign = sign(velocity.x)
+    if move_sign != 0:
+        visuals.scale = Vector2(move_sign, 1)
 
 
 func get_player_current():
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
-	return Vector2.ZERO
+    var player_node = get_tree().get_first_node_in_group("player") as Node2D
+    if player_node != null:
+        return (player_node.global_position - global_position).normalized()
+    return Vector2.ZERO
+
+
+func set_is_moving(moving: bool):
+    is_moving = moving
