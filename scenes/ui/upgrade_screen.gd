eae817dbf1387999b46fb19151152ddf568dd242
玄星 <2540149177@qq.com>
2024-05-28 15:08:55 +0800

    20. Animating the Upgrade Card

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -7,17 +7,20 @@ signal upgrade_selected(upgrade: AbilityUpgrade)
 @onready var card_container: HBoxContainer = $%CardContainer
 
 func _ready():
-    get_tree().paused = true
+	get_tree().paused = true
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
-    for upgrade in upgrades:
-        var card_instance = upgrade_card_sence.instantiate()
-        card_container.add_child(card_instance)
-        card_instance.set_ability_upgrade(upgrade)
-        card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
-        
+	var delay = 0
+	for upgrade in upgrades:
+		var card_instance = upgrade_card_sence.instantiate()
+		card_container.add_child(card_instance)
+		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+		delay += .2
+		
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
-    upgrade_selected.emit(upgrade)
-    get_tree().paused = false
-    queue_free()
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
